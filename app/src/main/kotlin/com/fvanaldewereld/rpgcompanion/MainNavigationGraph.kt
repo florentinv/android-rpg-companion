261b2278acbc83ff50639c5b7a80db0e86eaf2d3
Nicolas <125961563+NicoDKT@users.noreply.github.com>
2023-09-28 15:30:27 +0200

    fix: Navigation and animation finish callback (#26)
    Je te propose cette solution, j'ai mis des commentaires dans le code, libre à toi d'adapter comme tu le souhaite et de supprimer les commentaires ensuite :)

--- app/src/main/kotlin/com/fvanaldewereld/rpgcompanion/MainNavigationGraph.kt
+++ app/src/main/kotlin/com/fvanaldewereld/rpgcompanion/MainNavigationGraph.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.fvanaldewereld.rpgcompanion.home.feature.ui.component.HomeScreen
+import com.fvanaldewereld.rpgcompanion.splashScreen.feature.ui.component.BLOUPScreen
 import com.fvanaldewereld.rpgcompanion.splashScreen.feature.ui.component.SplashScreen
 import kotlinx.coroutines.delay
 
@@ -18,16 +20,16 @@ fun MainNavigationGraph(
         startDestination = MainNavigationRoute.Splash.route,
     ) {
         composable(route = MainNavigationRoute.Splash.route) {
-            LaunchedEffect(true) {
-                delay(5000)
+            SplashScreen {
+                // Quand l'animation est finie on peut passer à la suite
                 navHostController.popBackStack()
                 navHostController.navigate(MainNavigationRoute.Home.route)
             }
-            SplashScreen()
         }
 
         composable(route = MainNavigationRoute.Home.route) {
-            goToHomePage()
+            // Autant appeler directement la fonction composable, j'ai l'impression que passer par un intent faisait foirer le truc
+            HomeScreen { /* Gérer le onclick par un callback vers Main ou par l'appel d'une nouvelle route */ }
         }
     }
 }
--- app/src/main/kotlin/com/fvanaldewereld/rpgcompanion/splashScreen/feature/ui/component/SplashScreen.kt
+++ app/src/main/kotlin/com/fvanaldewereld/rpgcompanion/splashScreen/feature/ui/component/SplashScreen.kt
@@ -3,6 +3,7 @@ package com.fvanaldewereld.rpgcompanion.splashScreen.feature.ui.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -11,12 +12,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.fvanaldewereld.rpgcompanion.common.ui.theme.RPGCompanionTheme
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(onAnimationFinished : () -> Unit) {
     val composition by rememberLottieComposition(spec = LottieCompositionSpec.Asset("animation_ln0885pz.json"))
+    val progress by animateLottieCompositionAsState(composition)
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -27,6 +30,11 @@ fun SplashScreen() {
             iterations = LottieConstants.IterateForever,
             modifier = Modifier.fillMaxSize(0.7F),
         )
+        // Quand progress == 1.0 c'est qu el'animation est finie, on passe dans le callback
+        if(progress == 1.0f){
+            onAnimationFinished.invoke()
+        }
+
     }
 }
 
@@ -34,6 +42,6 @@ fun SplashScreen() {
 @Composable
 private fun SplashScreenPreview() {
     RPGCompanionTheme {
-        SplashScreen()
+        SplashScreen {}
     }
 }
